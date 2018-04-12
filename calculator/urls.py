3904fd6b9878d5bedd8b0c56becf9980ee80a923
RyanCunningham206 <Ryanguy7890@gmail.com>
2018-04-12 00:47:19 -0700

    Working site

--- calculator/urls.py
+++ calculator/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('calculate/', views.calculate, name='calculate'),
+    path('solution/', views.solution, name='solution'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+
+
+class Expiry:
+    def __init__(self, entries):
+        self.entries = entries
+
+    def finaldate(self):
+        today = datetime.datetime.now()
+        final_date = today + self.entries
+        return final_date
+
+
+weeks = 0
+days = 0
+hours = 0
+
+time = Expiry(datetime.timedelta(weeks=weeks, days=days, hours=hours))
+
 def home_page(request):
     return render(request, 'calculator/basic.html')
 
+
 def calculate(request):
-    print("haha")
-    return redirect("/")
+    global weeks
+    global days
+    global hours
+    if request.method == 'POST':
+        weeks = request.POST.get('weeks')
+        days = request.POST.get('days')
+        hours = request.POST.get('hours')
+    return redirect("/solution")
+
+def solution(request):
+    global weeks
+    print(weeks)
+    today = datetime.datetime.now()
+    final_date = today + datetime.timedelta(weeks=int(weeks), days=int(days), hours=int(hours))
+    print(final_date)
+    return HttpResponse(final_date)
+
+
 
